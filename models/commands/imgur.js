02d82f00f267b1c2ebfb5083adc6583b1a7ef0c8
mrjoy45555 <joyahmed56joy@gmail.com>
2025-05-12 23:40:52 +0600

    Update imgur.js

--- models/commands/imgur.js
+++ models/commands/imgur.js
@@ -12,12 +12,12 @@ module.exports.config = {
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];
 
-const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+const apis = await axios.get('https://raw.githubusercontent.com/JUBAED-AHMED-0JOY/Joy/main/api.json')
  const Shaon = apis.data.imgur
  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
  if(!linkanh) return api.sendMessage(' আপনি যেই ছোবি টাকে Imgur link বানাতে চান সেই ছোবি টা imgur লিখে রিপ্লাই করুন ', event.threadID, event.messageID)
-const res = await axios.get(`${Shaon}/imgur?link=${encodeURIComponent(linkanh)}`); 
+const res = await axios.get(`${Joy}/imgur?link=${encodeURIComponent(linkanh)}`); 
 var img = res.data.uploaded.image;
  return api.sendMessage(`"${img}",`, event.threadID, event.messageID);
 }
